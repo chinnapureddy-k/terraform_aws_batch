4cedf810abb5de1f782a6ffe5385bf24d10c1deb
Chinna <58542880+chinnapureddy-k@users.noreply.github.com>
2020-09-03 21:25:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  version = "3.1.0"
-  access_key = "your_access_key"
-  secret_key = "your_secret_key" 
+  version >= "3.1.0"
+  access_key = "AKIAUXSCQADYJLAF22RT"
+  secret_key = "zm1t70BwPxMmZw7s1YcWSA7HTOnEehVvn5LPoYwI" 
   region = "us-east-1"
 }
 
